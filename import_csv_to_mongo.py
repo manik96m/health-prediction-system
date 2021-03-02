3b8105664f6efd6a3cb86483cb8f762479ca361c
Ashly Chinnu Varghese <ashlycvarghese@gmail.com>
2021-03-02 17:22:07 -0500

    Added new Dataset

--- import_csv_to_mongo.py
+++ import_csv_to_mongo.py
@@ -1,4 +1,4 @@
-import sys
+
 import pandas as pd
 import pymongo
 import json
@@ -8,7 +8,7 @@ import os
 def import_content(filepath):
     mng_client = pymongo.MongoClient("mongodb+srv://admin:admin@cluster0.vwurs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
     mng_db = mng_client['diabetes_data'] # Replace mongo db name
-    collection_name = 'diabetes_collection' # Replace mongo db collection name
+    collection_name = 'Diabetes_US' # Replace mongo db collection name
     db_cm = mng_db[collection_name]
     cdir = os.path.dirname(__file__)
     file_res = os.path.join(cdir, filepath)
@@ -20,5 +20,5 @@ def import_content(filepath):
     print(db_cm.count_documents({}))
 
 if __name__ == "__main__":
-    filepath = 'diabetes_data_upload.csv'  # pass csv file path
+    filepath = 'diabetes.csv'  # pass csv file path
     import_content(filepath)
