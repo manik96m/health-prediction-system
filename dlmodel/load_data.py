4fc72b732ef17777fcdd148db891819b13a7a67f
Wen Dong <wen.dong@microfocus.com>
2021-03-26 07:38:55 +0800

    't'

--- dlmodel/load_data.py
+++ dlmodel/load_data.py
@@ -2,12 +2,12 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.utils import shuffle
 import pandas as pd
 import pickle
-import pymongo
 import json
 import os
 
 class LoadData:
     def __init__(self):
+        import pymongo
         xs = 'pickles/x_scaler.pkl'
         ys = 'pickles/y_scaler.pkl'
         with open(xs, 'rb') as pickle_file:
@@ -19,9 +19,6 @@ class LoadData:
             "mongodb+srv://admin:admin@cluster0.vwurs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
         db = mng_client.diabetes_data
         self.cursor = db.Diabetes_US.find({})
-
-    # def __iter__(self):
-    #     return self
 
     def next(self, batchsize=None):
         res = []
@@ -40,29 +37,6 @@ class LoadData:
         Y_scaled = self.Y_scaler.transform(Y_training)
 
         return X_scaled, Y_scaled
-
-def import_content(filepath):
-    # reading CSV and checking for null values
-    cdir = os.path.dirname(__file__)
-    file_res = os.path.join(cdir, filepath)
-    data = pd.read_csv(file_res)
-    # print(data.info())
-    if (data.isnull().sum().sum() != 0):
-        print('Error: Found null values in the dataset.')
-    else:
-        print('There are no null values in the dataset.')
-
-    # preparing database for upload
-    mng_client = pymongo.MongoClient("mongodb+srv://admin:admin@cluster0.vwurs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-    mng_db = mng_client['diabetes_data'] # Replace mongo db name
-    collection_name = 'Diabetes_US' # Replace mongo db collection name
-    db_cm = mng_db[collection_name]
-    data_json = json.loads(data.to_json(orient='records'))
-    db_cm.remove()
-
-    # inserting data to database
-    db_cm.insert(data_json)
-    print(db_cm.count_documents({}))
 
 def load_data(scaled=True):
     datafile = "../diabetes.csv"
@@ -105,7 +79,7 @@ def load_data(scaled=True):
 
 if __name__ == '__main__':
     # import_content('../diabetes.csv')
-    cursor = load_data_from_mongodb()
+    cursor = load_data()
     for x in cursor:
         print(x)
 
