7c2867c65d3c2d66a6269dac54c953339f7d6a5a
Wen Dong <wen.dong@microfocus.com>
2021-03-26 07:16:21 +0800

    't'

--- app.py
+++ app.py
@@ -36,16 +36,19 @@ def predict_json(project, model, instances, version=None):
     return response['predictions']
 
 def main(instance):
-    xs = 'dlmodel/pickles/x_scaler.pkl'
-    ys = 'dlmodel/pickles/y_scaler.pkl'
-    with open(xs, 'rb') as pickle_file:
-        X_scaler = pickle.load(pickle_file)
-    with open(ys, 'rb') as pickle_file:
-        Y_scaler = pickle.load(pickle_file)
+    # xs = 'dlmodel/pickles/x_scaler.pkl'
+    # ys = 'dlmodel/pickles/y_scaler.pkl'
+    # with open(xs, 'rb') as pickle_file:
+    #     X_scaler = pickle.load(pickle_file)
+    # with open(ys, 'rb') as pickle_file:
+    #     Y_scaler = pickle.load(pickle_file)
+    #
+    # # auth.authenticate_user()
+    #
+    # scaled_data = X_scaler.transform([instance]).tolist()
 
-    # auth.authenticate_user()
+    scaled_data = [[0.3529411764705882, 0.7437185929648241, 0.5901639344262295, 0.3535353535353536, 0.0, 0.5007451564828614,0.23441502988898377, 0.4833333333333334]]
 
-    scaled_data = X_scaler.transform([instance]).tolist()
     # print(instance)
     # print(scaled_data)
 
